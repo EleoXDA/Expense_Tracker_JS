3e7a54f4bee7b9afef547c46277f0e5943238a44
Elbay Malik <eleo.666@gmail.com>
2023-05-20 22:55:13 +0200

    Update script.js
    Add logic for adding income once

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 let income = 0;
 let expenses = [];
 
+document.getElementById('add-income').addEventListener('click', function() {
+    income = Number(document.getElementById('income').value);
+    updateIncomeInSummary();
+});
+
 document.getElementById('add-expense').addEventListener('click', function() {
     const expenseCategory = document.getElementById('expense-category').value;
     const expenseAmount = Number(document.getElementById('expense-amount').value);
     expenses.push({category: expenseCategory, amount: expenseAmount});
-    updateSummary();
+    updateExpensesInSummary();
 });
 
-function updateSummary() {
-    let totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
-    let balance = income - totalExpenses;
-    document.getElementById('summary-section').innerText = `Total Expenses: $${totalExpenses} \nBalance: $${balance}`;
+function updateIncomeInSummary() {
+    const table = document.getElementById('summary-table');
+    const row = table.insertRow(0);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    cell1.innerHTML = "Income";
+    cell2.innerHTML = "$" + income;
+}
+
+function updateExpensesInSummary() {
+    const table = document.getElementById('summary-table');
+    const row = table.insertRow(-1); // Inserts a row at the end of the table
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    cell1.innerHTML = expenses[expenses.length - 1].category;
+    cell2.innerHTML = "$" + expenses[expenses.length - 1].amount;
 }
